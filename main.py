ec8e6dc5e53fc0998e9761c41fe03da9c543162b
tcheol <andrew.swimmer@outlook.com>
2026-04-09 20:13:51 -0400

    up

--- main.py
+++ main.py
@@ -63,6 +63,30 @@ def main():
     dj.cleanup()
     cam.release()
     # tracker.close()   # uncomment when hand_tracker is ready
+    class HandTracker:
+        def __init__(self):
+            self.mpHands = mp.solutions.hands
+            self.hands = self.mpHands.Hands()
+            self.mpDraw = mp.solutions.drawing_utils
+
+        def find_hand(self, img):
+            imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            results = self.hands.process(imgRGB)
+
+            lm_list = []
+
+            if results.multi_hand_landmarks:
+                for handLms in results.multi_hand_landmarks:
+                    for id, lm in enumerate(handLms.landmark):
+                        h, w, c = img.shape
+                        cx, cy = int(lm.x * w), int(lm.y * h)
+                        lm_list.append((id, cx, cy))
+
+                        if id in [4,8,12,16,20]:
+                            cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+                        
+                    self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
+            return img, lm_list
 
 
 if __name__ == '__main__':
